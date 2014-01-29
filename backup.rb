1d2dfd42478d73663329222c851ed8e6f73266e5
Steve Marsh <Zimdale@gmail.com>
2014-01-29 07:54:37 -0500

    Modified todo in backup.rb

--- backup.rb
+++ backup.rb
@@ -5,7 +5,6 @@ time = Time.new
 #@TODO
 #check if directory exists.
 #loop through directory and back up to new directory
-#look into compression methods.
 saveDir ='backup/' +time.yday.to_s()+time.year.to_s()
 
 FileUtils.copy_entry my_dir, saveDir
